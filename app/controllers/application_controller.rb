0b325564530b70ab8d42b490218c916c9946c4e7
vagrant <vagrant@localhost.localdomain>
2020-07-04 11:28:32 +0000

    [update]Bookers_revel2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < ActionController::Base
-
+    # ユーザ登録・ログイン認証が行われる前に、「configure_permitted_parameters」を実行する！
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    # def after_sign_up_path_for(resource)
+    #     redirect_to user_path
+    # end
+      
+      #アカウント編集後のリダイレクト先
+    # def after_update_path_for(resource)
+    #     redirect_to user_path
+    # end
+
+    #ログイン後のリダイレクト先
+    # def after_sign_in_path_for(resource)
+    #     redirect_to user_path
+    # end 
+    #ログアウト後のリダイレクト先
+    # def after_sign_out_path_for(resource)
+    #     redirect_to user_path
+    # end 
 
     protected
     # サインアップをする際に、独自に追加した「name」カラムと、「email」カラムを許可する。
