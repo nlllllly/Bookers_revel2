a216a463464e95cde4e4171e9bd60f7dca9550a5
vagrant <vagrant@localhost.localdomain>
2020-07-05 02:38:35 +0000

    [update]Bookers_revel2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
     # ユーザ登録・ログイン認証が行われる前に、「configure_permitted_parameters」を実行する！
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    # def after_sign_up_path_for(resource)
-    #     redirect_to user_path
-    # end
+    #アカウント作成後のリダイレクト先
+    def after_sign_up_path_for(resource)
+      user_path(resource.id)
+    end
       
       #アカウント編集後のリダイレクト先
     # def after_update_path_for(resource)
@@ -12,9 +14,10 @@ class ApplicationController < ActionController::Base
     # end
 
     #ログイン後のリダイレクト先
-    # def after_sign_in_path_for(resource)
-    #     redirect_to user_path
-    # end 
+    def after_sign_in_path_for(resource)
+      user_path(resource.id)
+    end 
+
     #ログアウト後のリダイレクト先
     # def after_sign_out_path_for(resource)
     #     redirect_to user_path
