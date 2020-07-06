7c90bfb0bebf02cfccb5f2d5288eb276f9c106fa
vagrant <vagrant@localhost.localdomain>
2020-07-06 09:39:12 +0000

    [update]Bookers_revel2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-
-  end
   def index
     @users = User.all
+    @book = Book.new
+  end
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+  def create
+
+
   end
   def edit
     @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
   resource :home, only: [:index] do
     resources :about, only: [:index]
   end  
-  resources :users
+  resources :users, only: [:index, :show, :create, :edit, :update]
 end
